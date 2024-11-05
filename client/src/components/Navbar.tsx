98f659aaa4d88dda50fa3ba0a79550e44065d0f4
Eduardogl444 <eduardogl444@gmail.com>
2024-11-05 18:54:43 -0500

    search page and nav bar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,52 +1,20 @@
-import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import auth from '../utils/auth';
 
 const Navbar = () => {
-  // State to track the login status
-  const [loginCheck, setLoginCheck] = useState(false);
-
-  // Function to check if the user is logged in using auth.loggedIn() method
-  const checkLogin = () => {
-    if (auth.loggedIn()) {
-      setLoginCheck(true);  // Set loginCheck to true if user is logged in
-    }
-  };
-
-  // useEffect hook to run checkLogin() on component mount and when loginCheck state changes
-  useEffect(() => {
-    checkLogin();  // Call checkLogin() function to update loginCheck state
-  }, [loginCheck]);  // Dependency array ensures useEffect runs when loginCheck changes
-
   return (
     <div className="navbar display-flex justify-space-between align-center py-2 px-5 mint-green">
-      {
-        // Conditional rendering based on loginCheck state
-        !loginCheck ? (
-          // View when not logged in
-          <>
-            <h1>Authentication Review</h1>
-            <button className="btn" type='button'>
-              <Link to='/login'>Login</Link>
-            </button>
-          </>
-        ) : (
-        
-          <>
-            <div className="navbar-left">
-              <img src="/path-to-logo-icon.svg" alt="Logo" className="logo-icon" /> {'/icon.png'}
-              <span className="app-name">Nourish Mate</span>
-            </div>
-            <div className="navbar-right">
-              <Link to='/top-recipes' className="nav-link">Search Recipes</Link>
-              <Link to='/profile' className="nav-link">My Profile</Link>
-            </div>
-          </>
-        )
-      }
+      <div className="navbar-left">
+        {/* <img src="/path-to-logo-icon.svg" alt="Logo" className="logo-icon" /> */}
+        <span className="app-name">Nourish Mate</span>
+      </div>
+      <div className="navbar-right">
+        <Link to='/top-recipes' className="nav-link">Search Recipes</Link>
+        <Link to='/profile' className="nav-link">My Profile</Link>
+      </div>
     </div>
   );
 }
 
 export default Navbar;
+
 
